60e7d073fc72e48dd34c0e271c03d7e09d6541ef
Roy Montano <roymontano166@gmail.com>
2023-11-29 23:28:28 -0500

    Fin de sección 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Hola Mundo';    // Este title lo estoy llamando con interpolación en el component.html y esto solo se puede hacer dentro del mismo componente.
+
 }
